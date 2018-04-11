4b76982591bbb3cc4f1d8265e5cb487fb61074b9
嘻嘻哈哈 <wahwl@qq.com>
2018-04-11 22:43:14 +0800

    注册

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,8 @@
 const getters = {
   sidebar: state => state.app.sidebar,
-  token: state => state.user.token,
+  token: function(state) {
+    return state.app.token
+  },
   avatar: state => state.user.avatar,
   name: state => state.user.name,
   roles: state => state.user.roles
