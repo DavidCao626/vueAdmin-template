31db0b4d5ace2d3abcf6e8128aabeaae8ea3696e
嘻嘻哈哈 <wahwl@qq.com>
2018-05-04 10:22:51 +0800

    q

--- src/views/task/updateTaskProject.vue
+++ src/views/task/updateTaskProject.vue
@@ -31,75 +31,75 @@
 <script>
 	import { updateTaskProject, getTaskProject } from '~/api/task'
 
-	//这个是要修改的项目的项目编号
-	var systemSerialNo = "1001";
+	// 这个是要修改的项目的项目编号
+	var systemSerialNo = '1001';
 
 	var serviceTypeList = [{
-		'label': '业务类别',
-		'value': '1001'
-	}];
-	var formStore = {};
-	formStore.data = {
-		noteTitle: "", //节点标题
-		nodeDesc: "", //节点描述
-		planStartTime: "", //计划开始时间
-		planCompleteTime: "", //计划完成时间
-		projectNo: "", //项目编号
-		serviceTypeCode: "" //业务类别
+	  'label': '业务类别',
+	  'value': '1001'
+	}]
+var formStore = {}
+formStore.data = {
+	  noteTitle: '', // 节点标题
+	  nodeDesc: '', // 节点描述
+	  planStartTime: '', // 计划开始时间
+	  planCompleteTime: '', // 计划完成时间
+	  projectNo: '', // 项目编号
+	  serviceTypeCode: '' // 业务类别
 	}
 	formStore.rules = {
-		noteTitle: [], //节点标题
-		nodeDesc: [], //节点描述
-		planStartTime: [], //计划开始时间
-		planCompleteTime: [], //计划完成时间
-		projectNo: [], //项目编号
-		serviceTypeCode: [] //业务类别
+	  noteTitle: [], // 节点标题
+	  nodeDesc: [], // 节点描述
+	  planStartTime: [], // 计划开始时间
+	  planCompleteTime: [], // 计划完成时间
+	  projectNo: [], // 项目编号
+	  serviceTypeCode: [] // 业务类别
 	}
 	export default {
-		data() {
-			return {
-				formStore,
-				serviceTypeList //业务类别列表
-			}
-		},
-		methods: {
-			submitForm: function(formName) {
-				var data = this.formStore.data;
-				var that = this;
-				data.systemSerialNo = systemSerialNo;
-				this.$refs[formName].validate((valid) => {
-					if(valid) {
-						new Promise((resolve, reject) => {
-							updateTaskProject(data)
-								.then(response => {
-									 that.$message.success("成功!")
-									resolve(response)
-								})
-								.catch(error => {
-									reject(error)
-								})
-						})
-					} else {
-						return false;
-					}
-				});
-			}
-		},
-		mounted: function() {
-			new Promise((resolve, reject) => {
-                var data = {
-                    'systemSerialNo':systemSerialNo
-                }
-				getTaskProject(data)
-					.then(response => {
-						resolve(response)
-						console.log(["updateTaskProjectData",response]);
-					})
-					.catch(error => {
-						reject(error)
-					})
-			})
+	  data() {
+	    return {
+	      formStore,
+	      serviceTypeList // 业务类别列表
+	    }
+	  },
+	  methods: {
+	    submitForm: function(formName) {
+	      var data = this.formStore.data
+			var that = this
+			data.systemSerialNo = systemSerialNo
+			this.$refs[formName].validate((valid) => {
+	        if (valid) {
+	          new Promise((resolve, reject) => {
+	            updateTaskProject(data)
+	              .then(response => {
+									 that.$message.success('成功!')
+	                resolve(response)
+	              })
+	              .catch(error => {
+	                reject(error)
+	              })
+	          })
+	        } else {
+	          return false
+				}
+	      })
 		}
+	  },
+	  mounted: function() {
+	    new Promise((resolve, reject) => {
+      var data = {
+        'systemSerialNo': systemSerialNo
+      }
+	      getTaskProject(data)
+	        .then(response => {
+	          resolve(response)
+	          console.log(['updateTaskProjectData', response])
+				})
+	        .catch(error => {
+	          reject(error)
+	        })
+	    })
+	  }
 	}
 </script>
 
