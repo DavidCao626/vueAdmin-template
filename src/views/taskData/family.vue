0a34d696239db24ccf479d0a357e21f6d0b4020d
嘻嘻哈哈 <wahwl@qq.com>
2018-05-08 00:14:48 +0800

    whk

---
+++ src/views/taskData/family.vue
@@ -0,0 +1,78 @@
+<template>
+	<div>
+		<el-form :model="formData.data" :rules="formData.rules" ref="form1" size="mini" label-width="80px">
+			<el-form-item prop="familyPopulation" label="家庭人口数量">
+				<el-input-number v-model="formData.data.familyPopulation"></el-input-number>
+			</el-form-item>
+			<el-form-item prop="familyAddress" label="家庭住址">
+				<el-input v-model="formData.data.familyAddress"></el-input>
+			</el-form-item>
+			<el-form-item prop="familyCapitaincome" label="人均收入">
+				<el-input v-model="formData.data.familyCapitaincome"></el-input>
+			</el-form-item>
+			<el-form-item prop="naturalHazard" label="自然灾害">
+				<el-input v-model="formData.data.naturalHazard"></el-input>
+			</el-form-item>
+			<el-form-item prop="isSingleParent" label="是否单亲">
+				<el-radio-group v-model="formData.data.isSingleParent">
+					<el-radio-button v-for="(item,index) in isSingleParentData" :key="index" :label="item.value">{{item.label}}</el-radio-button>
+					<el-radio-button label="N"></el-radio-button>
+				</el-radio-group>
+			</el-form-item>
+			<el-form-item prop="anmeldenType" label="户口类型">
+				<el-select v-model="formData.data.anmeldenType">
+					<el-option v-for="(item,index) in anmeldenTypeList" :key="index" :label="item.label" :value="item.value">
+					</el-option>
+				</el-select>
+			</el-form-item>
+			<el-form-item prop="familyDebt" label="欠款金额">
+				<el-input v-model="formData.data.familyDebt"></el-input>
+			</el-form-item>
+		</el-form>
+	</div>
+</template>
+
+<script>
+	var isSingleParentData = [{
+		'label': '是',
+		'value': 'Y'
+	}, {
+		'label': '否',
+		'value': 'N'
+	}]
+
+	var anmeldenTypeList = [{
+		'label': '农村',
+		'value': 'nc'
+	}, {
+		'label': '城镇',
+		'value': 'cz'
+	}]
+
+	export default {
+		data() {
+			return {
+				isSingleParentData,
+				anmeldenTypeList,
+				formData: {
+					data: {
+						familyPopulation: 0,
+						familyAddress: '',
+						familyCapitaincome: '',
+						naturalHazard: '',
+						isSingleParent: 'N',
+						anmeldenType: '',
+						familyDebt: ''
+					},
+					rules: {
+
+					}
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/taskData/projectApply.vue
@@ -0,0 +1,67 @@
+<template>
+	<div>
+		
+		<el-form :model="formData.data" :rules="formData.rules" ref="form1" label-width="" size="mini">
+			<el-form-item prop="applyProject" label="项目名称">
+				<el-input v-model="formData.data.appkyProject"></el-input>
+			</el-form-item>
+			<el-form-item prop="serviceType" label="业务类别">
+				<el-select v-model="formData.data.serviceType">
+					<el-option v-for="(item,index) in anmeldenTypeList" :key="index" :label="item.label" :value="item.value">
+					</el-option>
+				</el-select>
+			</el-form-item>
+			<el-form-item prop="childServiceType" label="子业务类别">
+				<el-select v-model="formData.data.childServiceType">
+					<el-option v-for="(item,index) in anmeldenTypeList" :key="index" :label="item.label" :value="item.value">
+					</el-option>
+				</el-select>
+			</el-form-item>
+			<el-form-item prop="evaluationType" label="关联测评类别">
+				<el-select v-model="formData.data.evaluationType">
+					<el-option v-for="(item,index) in anmeldenTypeList" :key="index" :label="item.label" :value="item.value">
+					</el-option>
+				</el-select>
+			</el-form-item>
+			<el-form-item prop="applyReason" label="申请理由">
+				<el-input type="textarea" v-model="formData.data.applyReason"></el-input>
+			</el-form-item>
+		</el-form>
+		
+	</div>
+</template>
+
+<script>
+		var anmeldenTypeList = [{
+		'label': '数据',
+		'value': 'nc'
+	}, {
+		'label': '数据',
+		'value': 'cz'
+	}]
+	export default{
+        data(){
+            return {
+        anmeldenTypeList,
+				formData:{
+					data:{
+						applyProject:"",
+						serviceType:"",
+						childServiceType:"",
+						evaluationType:"",
+						applyReason:""
+					},
+					rules:{
+						
+					}
+				}
+				
+				
+			}
+		}
+		
+	}
+</script>
+
+<style>
+</style>
