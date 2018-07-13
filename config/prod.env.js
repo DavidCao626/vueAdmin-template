0f65aced53dea657d12da9bcf59a140aee357eb9
chengbinTechnology <definedPerson@163.com>
2018-07-13 09:10:45 +0800

    comiit

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,5 +4,5 @@ const merge = require("webpack-merge");
 const prodEnv = require("./prod.env");
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://39.105.132.61:8080/StuService"'
+  BASE_API: '"StuService"'
 };
