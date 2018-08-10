3b6277dd265febd08ec7456dabfc94ca73f19cc7
DavidCao <davidcao@DavidCaodeiMac.local>
2018-08-10 18:19:55 +0800

    科目分值模版更新

--- src/directive/state-show/state.js
+++ src/directive/state-show/state.js
@@ -1,12 +1,31 @@
 export default {
+  getVShowValue(vnode) {
+    let values = vnode.data.directives.filter(function(s) {
+      return s.rawName == "v-show";
+    });
+  },
   inserted(el, binding, vnode) {
-    let cnodeNumber =
-      vnode.context.ShowStateBit ||
-      vnode.context.$root.rootShowStateBit ||
-          0; //找到当前所在组件区域rootShowStateBit
+   
+    let values = vnode.data.directives.filter(function(s) {
+      return s.rawName == "v-show";
+    });
+    let bool;
 
-    if ((cnodeNumber & binding.value) == 0) {
-      el.style.display = "none";
+    if (values && values.length > 0) {
+      bool = values[0].value;
+    } else {
+      bool = true;
+    }
+    if (bool) {
+      let cnodeNumber =
+        vnode.context.ShowStateBit ||
+        vnode.context.showStateBit ||
+        vnode.context.$root.rootShowStateBit ||
+        0; //找到当前所在组件区域rootShowStateBit
+
+      if ((cnodeNumber & binding.value) == 0) {
+        el.style.display = "none";
+      }
     }
   }
 };
