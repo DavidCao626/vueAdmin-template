2afc75cda0c6b413bdfc4a677081d1db6b2da2ac
DavidCao <davidcao@DavidCaodeiMac.local>
2018-08-14 01:13:28 +0800

    修改状态问题

--- src/views/zongce/pingce.vue
+++ src/views/zongce/pingce.vue
@@ -7,7 +7,7 @@
             <div slot="extra">
                 <el-input placeholder="搜索学生姓名或学号" prefix-icon="el-icon-search">
                 </el-input>
-                 <el-button plain>搜索</el-button>
+                <el-button plain>搜索</el-button>
             </div>
             <div class="approval-panel">
                 <p>　　加强民主评议政风行风工作，对于纠正部门和行业不正之风、促进依法行政、推进党风廉政建设、优化发展环境具有重要作用。近年来，承蒙社会各界的高度关注和广大人民群众的热情参与，我县政风行风评议工作取得较好成效，希望继续关注和积极参与我县评议工作，请您根据被评议单位在依法履职、规范服务、办事效率、转变作风等方面的情况，给予客观公正评价。为有效杜绝恶意评议，评议为不满意单位不得超过60%，即63个单位，否则不予提交评议结果。本次测评采用了限制IP投票，每个固定IP在72小时内限投参与一次。网上评议起止日期为2017年9月18日0:00至2017年10月18日24:00</p>
@@ -66,7 +66,7 @@ export default {
 
 <style scoped>
 .el-input {
-  width: auto;
+  width: 50%;
 }
 .item-head {
 }
