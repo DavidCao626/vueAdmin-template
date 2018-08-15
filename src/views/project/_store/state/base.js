d6258508e77831a63ff002256fda7ba8afeb2dd5
嘻嘻哈哈 <wahwl@qq.com>
2018-08-15 15:02:01 +0800

    whkCommit

--- src/views/project/_store/state/base.js
+++ src/views/project/_store/state/base.js
@@ -14,12 +14,19 @@ export const baseproject = {
         projectAttachmentId: [],
         attrDetailBean: [],
         expand: {
-            appraiseId:"",
+            appraiseId:0,
             appraiseName: "",//名称
             appraiseStartTime: "",//开始时间
             appraiseEndTime: "",//结束时间
             appraiseYearType: "2018",//年度
             appraiseServiceType:""//测评类别
+        },
+        resultExpand: {
+            id:0,
+            appraiseProjectCode: "",//测评项目代码
+            assessSubjects: "",//考评科目(无科目项)
+            collegeSubjects: [],//学院可管理科目
+            classSubjects:[]//班级可管理科目
         }
     },
     tempProjectId:"",//临时储存当前项目id
