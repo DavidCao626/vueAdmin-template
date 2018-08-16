b465b9d65bef7cfcf8c1f6e1e13e1dd7510066ac
DavidCao <davidcao@DavidCaodeiMac.local>
2018-08-16 19:46:22 +0800

    elxselcet增强

---
+++ src/components/Select/test.vue
@@ -0,0 +1,45 @@
+<template>
+    <elx-select v-model="value" placeholder="请选择" @change="changeValue">
+        <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+        </el-option>
+    </elx-select>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕"
+        },
+        {
+          value: "选项2",
+          label: "双皮奶"
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎"
+        },
+        {
+          value: "选项4",
+          label: "龙须面"
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭"
+        }
+      ],
+      value: ""
+    };
+  },
+  methods: {
+    changeValue(value,o) {
+      console.log(o);
+      
+      debugger
+    }
+  }
+};
+</script>
