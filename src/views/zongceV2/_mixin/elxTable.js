2a1a89d405ac35c0073eb8f589c2dc49c038fca8
DavidCao <davidcao@DavidCaodeiMac.local>
2018-09-14 16:39:53 +0800

    mysubjectCode

--- src/views/zongceV2/_mixin/elxTable.js
+++ src/views/zongceV2/_mixin/elxTable.js
@@ -13,7 +13,56 @@ export default {
       newOpen: false,
       data: [],
       action: "https://jsonplaceholder.typicode.com/posts/",
-      loading: true
+      loading: true,
+      pickerOptions2: {
+        shortcuts: [
+          {
+            text: "最近一周",
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit("pick", [start, end]);
+            }
+          },
+          {
+            text: "最近一个月",
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit("pick", [start, end]);
+            }
+          },
+          {
+            text: "最近三个月",
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+              picker.$emit("pick", [start, end]);
+            }
+          },
+          {
+            text: "最近一年",
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 365);
+              picker.$emit("pick", [start, end]);
+            }
+          },
+          {
+            text: "最近三年",
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 365 * 3);
+              picker.$emit("pick", [start, end]);
+            }
+          }
+        ]
+      }
     };
   },
   methods: {
--- src/views/zongceV2/stuPunish/myPunish.vue
+++ src/views/zongceV2/stuPunish/myPunish.vue
@@ -3,42 +3,28 @@
         <div slot="title">我的处分查询</div>
         <div slot="panel">
             <div>
-                <el-dialog title="导入数据" :visible.sync="dialogVisible" width="400px" :before-close="handleClose">
-                    <el-upload class="upload-demo" drag :action="action" :limit='1' @onSuccess="onUploadSuccess">
-                        <i class="el-icon-upload"></i>
-                        <div class="el-upload__text">将文件拖到此处，或
-                            <em>点击上传</em>
-                        </div>
-                        <div class="el-upload__tip" slot="tip">只能上传xlx/xlsx</div>
-                    </el-upload>
-                </el-dialog>
 
                 <elx-table-layout>
-                    <template slot="headerRight">
-                        <el-button-group>
-                            <el-tooltip class="item" effect="dark" content="导入数据" placement="bottom" v-if="importOpen">
-                                <el-button plain size="mini" @click="dialogVisible = true">
-                                    <i class="el-icon-download"></i>
-                                </el-button>
-                            </el-tooltip>
-                            <el-tooltip class="item" effect="dark" content="导出数据" placement="bottom" v-if="exportOpen">
-                                <el-button plain size="mini" @click="onExportExcel">
-                                    <i class="el-icon-upload2"></i>
-                                </el-button>
-                            </el-tooltip>
-                        </el-button-group>
-                    </template>
 
                     <template slot="headerLeft">
-                        <span v-if="deleteOpen && isMultipleSelection">
-                            <el-button plain @click="onMultipleSelectionDel" size="mini" style="margin-top: 1px;margin-right: 20px;">
-                                <i class="el-icon-delete"> ({{ multipleSelection.length }})</i>
-                            </el-button>
-                        </span>
+
                         <el-form label-position="left" :inline="true" :model="formInline" size="mini" label-width="80px" class="demo-form-inline">
-                            
+
                             <el-form-item label="所属学年:">
-                                <el-input v-model="formInline.user" placeholder="学号"></el-input>
+                                <el-select v-model="formInline.schoolYearId" placeholder="全部">
+                                    <el-option v-for="(item,i) in schoolYearDict" :key="i" :label="item.name" :value="item.id">
+                                    </el-option>
+                                </el-select>
+                            </el-form-item>
+                            <el-form-item label="相关科目:">
+                                <el-select v-model="formInline.subjectCode" placeholder="全部">
+                                    <el-option v-for="(km,k) in schoolKm" :key="k" :label="km.name" :value="km.code">
+                                    </el-option>
+                                </el-select>
+                            </el-form-item>
+                            <el-form-item label="筛选范围:">
+                                <el-date-picker v-model="formInline.value" type="daterange" align="right" unlink-panels range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" :picker-options="pickerOptions2">
+                                </el-date-picker>
                             </el-form-item>
 
                             <el-form-item>
@@ -48,40 +34,34 @@
                     </template>
 
                     <el-table :data="data" style="width: 100%" border size="mini" :default-sort="{prop: 'date', prop: 'name',prop: 'address'}" @selection-change="handleSelectionChange">
-                        <el-table-column type="selection" width="38" v-if="deleteOpen">
+
+                        <el-table-column prop="stuNo" sortable label="学号">
                         </el-table-column>
 
-                        <el-table-column prop="name" sortable label="学生姓名">
+                        <el-table-column prop="stuName" sortable label="学生姓名">
                         </el-table-column>
-                        <el-table-column prop="date" sortable label="学号">
+                        <el-table-column prop="subjectName" label="科目名称">
                         </el-table-column>
-
-                        <el-table-column prop="address" label="所属学年">
+                        <el-table-column prop="schoolYearName" label="所属学年">
                         </el-table-column>
-                        <el-table-column prop="address" label="分值">
+                        <el-table-column prop="subjectScore" label="分值">
                         </el-table-column>
-
-                        <!-- <el-table-column type="expand" label="#" width="42">
-                            <template slot-scope="props" style="background-color:#f7f8f9">
-                                <el-form label-position="left" inline class="demo-table-expand">
-                                    <el-form-item label="身份证号码:">
-                                        <span>{{ props.row.name }}</span>
-                                    </el-form-item>
-                                    <br/>
-                                    <el-form-item label="高考成绩:">
-                                        <span>{{ props.row.category }}</span>
-                                    </el-form-item>
-                                    <br/>
-                                    <el-form-item label="备注资料:">
-                                        <span>{{ props.row.category }}</span>
-                                    </el-form-item>
-                                </el-form>
+                        <el-table-column prop="happenTime" label="发生时间">
+                        </el-table-column>
+                        <el-table-column prop="matterDesc" label="事件描述">
+                        </el-table-column>
+                        <el-table-column prop="orgName" label="组织名称">
+                        </el-table-column>
+                        <el-table-column prop="attrPath" label="相关附件">
+                            <template slot-scope="scope">
+                                <a :href="scope.row.attrPath" target="_blank">
+                                    <i class="el-icon-download"></i> 下载</a>
                             </template>
-                        </el-table-column> -->
+                        </el-table-column>
                     </el-table>
 
                     <template slot="footer">
-                        <el-pagination :page-size="100" layout="prev, pager, next, jumper" :total="1000">
+                        <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pageInfo.currentPage" :page-sizes="[10, 20, 50, 100]" :page-size="pageInfo.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="pageInfo.totalRecord">
                         </el-pagination>
                     </template>
 
@@ -93,88 +73,55 @@
 </template>
 
 <script>
+import elxTable from "../_mixin/elxTable.js";
+import store from "../_mixin/store.js";
 export default {
+  mixins: [elxTable, store],
   data() {
     return {
-      multipleSelection: [], //选中的值
-      isMultipleSelection: false, //是否选中
-
-      dialogVisible: false,
-      deleteOpen: false,
       importOpen: false,
-      exportOpen: false,
-      newOpen: false,
-      data: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ],
+      schoolYearDict: [],
+      schoolKm: [],
       formInline: {
-        user: "",
-        region: ""
-      },
-      action: "https://jsonplaceholder.typicode.com/posts/"
+        subjectCode: "",
+        schoolYearId: "",
+        startTime: "",
+        endTime: "",
+        value: []
+      }
     };
   },
-  watch: {
-    multipleSelection() {
-      return this.multipleSelection.length > 0
-        ? (this.isMultipleSelection = true)
-        : (this.isMultipleSelection = false);
-    }
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      vm.getSchoolYearDict();
+      vm.getData();
+    });
   },
   methods: {
-    handleClose(done) {
-      this.$confirm("确认关闭？")
-        .then(_ => {
-          done();
-        })
-        .catch(_ => {});
-    },
-    onSubmit() {
-      console.log("submit!");
-    },
-    handleSelectionChange(val) {
-      this.multipleSelection = val;
-    },
-    onMultipleSelectionDel() {
-      this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning"
-      }).then(() => {
-        this.$message({
-          type: "success",
-          message: "删除成功!"
-        });
-        this.$emit("onSelectionDel", this.multipleSelection);
+    getSchoolYearDict() {
+      this.querySchoolYearDict({}).then(response => {
+        this.loading = false;
+        this.schoolYearDict = response.resBody;
+        this.schoolYearDict.unshift({ id: 0, name: "全部" });
+        this.getPunishItemByShoolYearId(this.formInline.schoolYearId);
       });
     },
-    onExportExcel() {
-      this.$emit("onExportExcel");
+    getPunishItemByShoolYearId(id) {
+      this.queryPunishItemByShoolYearId({
+        schoolYearId: id
+      }).then(response => {
+        this.loading = false;
+        this.schoolKm = response.resBody.itemBeans;
+        this.schoolKm.unshift({ code: 0, name: "全部" });
+      });
     },
-    onUploadSuccess() {
-      this.$emit("onUploadSuccess");
-    },
-    onNew() {
-      this.$emit("onNew");
+    getData() {
+      this.getApi(this.queryPunishRecordForStu, {
+        subjectCode: this.formInline.subjectCode,
+        schoolYearId: this.formInline.schoolYearId,
+        startTime: Date.parse(this.formInline.value[0]) || "",
+        endTime: Date.parse(this.formInline.value[1]) || ""
+      });
     }
   }
 };
